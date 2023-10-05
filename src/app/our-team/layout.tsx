e3f2d4574395c889ab1225785243f89dea6af7d3
Ramazani <ramazanimwemedi234@gmail.com>
2023-10-05 16:43:11 +0300

    /

--- src/app/our-team/layout.tsx
+++ src/app/our-team/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
-
 const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SIR | Our Board",
+  title: "SIR | Our Team",
   description: "",
   icons: { icon: "/sirafrica_logo.png" },
 };
