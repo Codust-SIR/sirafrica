8669b8cee472dff3abde28178e96e248852359bd
Ramazani <ramazanimwemedi234@gmail.com>
2023-11-02 13:43:01 +0300

    Modied the missin=on and vission statement on the mission and vision page

--- src/app/our-mission-vision/page.tsx
+++ src/app/our-mission-vision/page.tsx
@@ -1,46 +1,30 @@
 "use client";
 
 import Image from "next/image";
-import "react-multi-carousel/lib/styles.css";
 import HideAppBar from "../../../components/AppDawer";
 import {
   Box,
-  Button,
   CssBaseline,
   ThemeProvider,
   Typography,
   createTheme,
   useMediaQuery,
-  Link,
-  Card,
-  CardMedia,
-  CardContent,
-  useTheme,
 } from "@mui/material";
-import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 import { useMemo } from "react";
-import EastRoundedIcon from "@mui/icons-material/EastRounded";
-import { styled } from "@mui/system";
-import Grid from "@mui/material/Unstable_Grid2/Grid2";
-import VolunteerActivismRoundedIcon from "@mui/icons-material/VolunteerActivismRounded";
 
 export default function Donate() {
-  const theme = useMemo(
-    () =>
-      createTheme({
-        typography: {
-          fontFamily: "inherit",
-        },
-      }),
-    []
-  );
+  const theme = createTheme({
+    typography: {
+      fontFamily: "inherit",
+    },
+  });
   const isMobileView = useMediaQuery(() => theme.breakpoints.down("sm"));
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <HideAppBar>
-        <Box p={isMobileView ? 1 : 15} pt={1}>
+        <Box p={isMobileView ? 1 : 8} pt={1}>
           <Typography variant="h3">Mission and Vision</Typography>
           <br />
           <Box
@@ -69,22 +53,24 @@ export default function Donate() {
                   width: isMobileView ? "100%" : "80%",
                 }}
               >
-                We aim to enhance/improve people’s lives through Education and
-                livelihood with the use of digital Technology as a major key to
-                influence positive change in order to guarantee more human
-                conditions to all living beings.
+                Our vision is a world where refugees, despite the challenges
+                they face, have the opportunity to lead fulfilling lives,
+                contribute to their communities, and reach their full potential.
               </Typography>
             </Box>
             <Box flex={0.5}>
+              {" "}
+              <br />
+              <br />
+              <br />
               <Image
-                src={
-                  "https://images.pexels.com/photos/6476602/pexels-photo-6476602.jpeg?auto=compress&cs=tinysrgb&w=1200"
-                }
-                height={500}
+                src={"/img21.jpeg"}
+                height={1500}
                 width={1200}
                 style={{
-                  borderRadius: 1,
                   width: isMobileView ? "100%" : "auto",
+                  height: isMobileView ? "auto" : 500,
+                  borderRadius: 15,
                 }}
                 alt="Image"
               />
@@ -104,15 +90,18 @@ export default function Donate() {
           >
             {!isMobileView && (
               <Box flex={0.5}>
+                {" "}
+                <br />
+                <br />
+                <br />
                 <Image
-                  src={
-                    "https://images.pexels.com/photos/6476602/pexels-photo-6476602.jpeg?auto=compress&cs=tinysrgb&w=1200"
-                  }
-                  height={500}
+                  src={"/img22.jpeg"}
+                  height={1500}
                   width={1200}
                   style={{
-                    borderRadius: 1,
                     width: isMobileView ? "100%" : "auto",
+                    height: isMobileView ? "auto" : 500,
+                    borderRadius: 15,
                   }}
                   alt="Image"
                 />
@@ -134,22 +123,25 @@ export default function Donate() {
                   width: isMobileView ? "100%" : "80%",
                 }}
               >
-                Our mission is to support youth (both men and women equally)
-                back to the path of regaining what insecurity, discrimination,
-                war, and disaster took away from them.
+                SIR is dedicated to empowering refugees and locals through
+                innovative programs and advocacy efforts that enhance their
+                access to quality education, and dignified livelihood
+                opportunities.
               </Typography>
             </Box>
             {isMobileView && (
               <Box flex={0.5}>
+                <br />
+                <br />
+                <br />
                 <Image
-                  src={
-                    "https://images.pexels.com/photos/6476602/pexels-photo-6476602.jpeg?auto=compress&cs=tinysrgb&w=1200"
-                  }
-                  height={500}
+                  src={"/img22.jpeg"}
+                  height={1500}
                   width={1200}
                   style={{
-                    borderRadius: 1,
                     width: isMobileView ? "100%" : "auto",
+                    height: isMobileView ? "auto" : 500,
+                    borderRadius: 15,
                   }}
                   alt="Image"
                 />
@@ -159,156 +151,5 @@ export default function Donate() {
         </Box>
       </HideAppBar>
     </ThemeProvider>
-  );
-}
-
-const CenteredBox = styled(Box)`
-  display: grid;
-  height: auto;
-  text-align: center;
-`;
-function MediaCard() {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        style={{ maxHeight: 140, width: "100%", objectFit: "cover" }}
-        image="https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&w=600"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-}
-function StoryBlogCard() {
-  const theme = useTheme();
-  const isMobileView = useMediaQuery(() => theme.breakpoints.down("sm"));
-  return (
-    <Card sx={{ maxWidth: 670, display: isMobileView ? "block" : "flex" }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="280"
-        style={{ maxHeight: 280, width: "100%", objectFit: "cover" }}
-        image="https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&w=600"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-        <Button
-          color="success"
-          sx={{
-            textTransform: "none",
-            borderRadius: 2,
-            mt: 3,
-          }}
-          variant="contained"
-          endIcon={<EastRoundedIcon />}
-        >
-          Read more
-        </Button>
-      </CardContent>
-    </Card>
-  );
-}
-
-function Parterns() {
-  const theme = useTheme();
-  const isMobileView = useMediaQuery(() => theme.breakpoints.down("sm"));
-  const partners: { name: string; url: string; image: string }[] = [
-    {
-      name: "Source Humanitarian Network",
-      url: "https://source-network.org/",
-      image: "/shn_logo.png",
-    },
-    {
-      name: "Humanitarian OpenStreetMap Team",
-      url: "",
-      image: "/hot_logo.png",
-    },
-    {
-      name: "WUSC",
-      url: "",
-      image: "/wusc_logo.png",
-    },
-    {
-      name: "Don Bosco Kakuma",
-      url: "",
-      image: "/donbosco_logo.png",
-    },
-    {
-      name: "Danish Refugees Council",
-      url: "",
-      image: "/drc_logo.jpg",
-    },
-    {
-      name: "Cohere",
-      url: "",
-      image: "/cohere_logo.png",
-    },
-    {
-      name: "GIZ",
-      url: "",
-      image: "/giz_logo.jpeg",
-    },
-  ];
-  return (
-    <Box sx={{ flexGrow: 1, p: 2 }}>
-      <Grid
-        container
-        spacing={2}
-        sx={{
-          "--Grid-borderWidth": "1px",
-          borderTop: "var(--Grid-borderWidth) solid",
-          borderLeft: "var(--Grid-borderWidth) solid",
-          borderColor: "divider",
-          "& > div": {
-            borderRight: "var(--Grid-borderWidth) solid",
-            borderBottom: "var(--Grid-borderWidth) solid",
-            borderColor: "divider",
-          },
-        }}
-      >
-        {partners.map((item, index) => (
-          <Grid
-            key={index}
-            {...{ xs: 12, sm: 6, md: 4, lg: 3 }}
-            minHeight={160}
-          >
-            <Image
-              src={item.image}
-              height={500}
-              width={500}
-              style={{
-                borderRadius: 1,
-                width: isMobileView ? "100%" : "auto",
-                maxHeight: isMobileView ? 90 : 90,
-                maxWidth: isMobileView ? 90 : 250,
-              }}
-              alt="Image"
-            />
-            <Typography variant="subtitle2" color={"text.secondary"}>
-              {item.name}
-            </Typography>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
   );
 }
