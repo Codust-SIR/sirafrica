3ee3c508181ef0463c71350007d6a2a10c0fad21
Ramazani Mwemedi <75116349+RamazaniMwemedi@users.noreply.github.com>
2024-07-05 04:41:36 +0300

    styles

--- components/DonateCard.tsx
+++ components/DonateCard.tsx
@@ -5,13 +5,18 @@ const DonateCard = () => {
     <iframe
       src="https://donorbox.org/embed/join-our-cause-4?default_interval=m&amount=30&hide_donation_meter=true"
       name="donorbox"
-      allowpaymentrequest
-      style="border: none; max-width: 500px; min-width: 250px; max-height: none!important;"
+      allowPaymentRequest
+      style={{
+        border: "none",
+        maxWidth: "500px",
+        minWidth: "250px",
+        maxHeight: "none!important",
+      }}
       scrolling="no"
       height="900"
       width="100%"
       allow="payment"
-    ></iframe>
+    />
   );
 };
 
